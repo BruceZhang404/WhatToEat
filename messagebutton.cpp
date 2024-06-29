54dfa2b8cda8d306e106d274a2f70628a01324f5
Bruce Zhang <143377484+BruceZhang404@users.noreply.github.com>
2024-06-29 14:29:07 +0800

    Delete messagebutton.cpp

--- messagebutton.cpp
+++
@@ -1,9 +0,0 @@
-#include "messagebutton.h"
-
-MessageButton::MessageButton(QWidget *parent):QPushButton(parent) {
-    connect(this,&QPushButton::clicked,this,&MessageButton::goDetail);
-}
-void MessageButton::goDetail(){
-    //qDebug()<<this->dish_id;
-    emit sendId(this->dish_id);
-}
